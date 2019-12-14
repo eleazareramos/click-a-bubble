7d0e3dd4ecdfb441375effc6d027d223801d8a06
Eleazar Ramos <e@fureyfs.com>
2019-12-14 13:24:27 -0500

    set up styles

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react'
+import '../styles/global.css'
+
+const Index = props => {
+  return (
+    <div className='main'>
+      
+    </div>
+  )
+}
+
+export default Index
