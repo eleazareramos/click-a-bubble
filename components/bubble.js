a07ceb30dcbc7489a3c5f0dde4f179c1cdfe34e5
Amir Sigari <asigari0711@gmail.com>
2019-12-14 15:28:47 -0500

    color changer

--- components/bubble.js
+++ components/bubble.js
@@ -1,14 +1,30 @@
-import React, {useState, useEffect} from 'react'
-import '../styles/bubble.css'
+import React, { useState, useEffect } from "react";
+import "../styles/bubble.css";
 
-const Bubble = props =>{
+const Bubble = props => {
+const [color, setColor] = useState('red')
+const [interval, intervalSet] = useState(2000)
 
-    return( 
 
-         <div className='bubble'>
-             hello 
-             </div>
-        )
-}
+  const randNum = () => Math.floor(Math.random() * 4)
+  const colors = [
+      'red',
+      'yellow',
+      'blue',
+      'green'
+  ]
+  useEffect(() => {
+      setInterval(function changeColor() {
+        setColor(colors[randNum()]);
+        console.log(color)
+        intervalSet(interval - 50)
+        console.log(interval)
+      }, interval);
+      
 
-export default Bubble
+  }, [])
+
+  return (<div className="bubble" style = {{backgroundColor:color}}>hello</div>);
+};
+
+export default Bubble;
