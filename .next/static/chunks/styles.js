599b91dab0096409307c7ba5cffa6ddd6767608b
Amir Sigari <asigari0711@gmail.com>
2019-12-14 13:59:19 -0500

    another

--- .next/static/chunks/styles.js
+++ .next/static/chunks/styles.js
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1576348958513");
+            injectCss(link, link.href.split("?")[0] + "?unix=1576349612562");
           });
       }
     }
@@ -66,7 +66,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1576348777810");
+            injectCss(link, link.href.split("?")[0] + "?unix=1576349612543");
           });
       }
     }
--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8b30aa716bc6571fc049";
+/******/ 	var hotCurrentHash = "71129c5dbc0a630341c0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
