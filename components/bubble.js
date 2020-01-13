a7d6004159580c45231ed5ca254bbe31e2981647
David Roghanian <dave@fureyfs.com>
2020-01-13 17:55:33 -0500

    dave branch

--- components/bubble.js
+++ components/bubble.js
@@ -1,14 +1,46 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import '../styles/bubble.css'
 
-const Bubble = props =>{
+const Bubble = props => {
+  const [color, setColor] = useState('red')
+  const [size, setSize] = useState(100)
+  const [gameOver, setGameOver] = useState(false)
+  const [text, setText] = useState('')
 
-    return( 
+  const handleClick = () => {
+    if (color == 'red') {
+      setSize(size + 50)
+    }
+  }
 
-         <div className='bubble'>
-             hello 
-             </div>
-        )
+  useEffect(() => {
+    if (size >= 500) {
+      setGameOver(true)
+      setText('You win!')
+    }
+  }, [size])
+
+  return (
+    <div
+      className="bubble"
+      onClick={handleClick}
+      style={
+        gameOver
+          ? {
+              height: 50,
+              width: 50,
+              backgroundColor: 'green'
+            }
+          : {
+              height: size,
+              width: size,
+              backgroundColor: color
+            }
+      }
+    >
+      {text}
+    </div>
+  )
 }
 
 export default Bubble
